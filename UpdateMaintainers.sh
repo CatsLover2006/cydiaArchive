10817e2681d76326a98b7abcf93d17176463c5ad
CatsLover2006 <chancelorne@icloud.com>
2024-01-24 10:47:18 -0800

    added sbsettings

--- UpdateMaintainers.sh
+++ UpdateMaintainers.sh
@@ -6,27 +6,15 @@
 #  Created by Half-Qilin on 2024-01-23.
 #
 
+if [ -z "$1" ]; then
+    dir='./'
+else
+    dir=$1
+fi
+
 rm -rf ./tmp
 
-for i in *.deb; do
-    [ -f "$i" ] || break
-    echo "File: $i"
-    dpkg-deb -R "$i" tmp
-    python3 UpdateMaintainerFile.py
-    dpkg-deb -b tmp "$i"
-    rm -rf ./tmp
-done
-
-for i in */*.deb; do
-    [ -f "$i" ] || break
-    echo "File: $i"
-    dpkg-deb -R "$i" tmp
-    python3 UpdateMaintainerFile.py
-    dpkg-deb -b tmp "$i"
-    rm -rf ./tmp
-done
-
-for i in */*/*.deb; do
+for i in $dir/*.deb; do
     [ -f "$i" ] || break
     echo "File: $i"
     dpkg-deb -R "$i" tmp
