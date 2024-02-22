6a7ab6991f33943fdd826742123bbbad7b6ff3bc
CatsLover2006 <chancelorne@icloud.com>
2024-02-21 23:12:17 -0800

    Hopefully this fixes install issues

---
+++ UpdateAllMaintainers.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#  UpdatePackages.sh
+#
+#
+#  Created by Half-Qilin on 2024-01-23.
+#
+
+if [ -z "$1" ]; then
+dir='./'
+else
+dir=$1
+fi
+
+rm -rf ./tmp
+
+for i in $dir/*/*.deb; do
+    [ -f "$i" ] || break
+    echo "File: $i"
+    dpkg-deb -R "$i" tmp
+    python3 ./UpdateMaintainerFile.py
+    dpkg-deb -b tmp "$i"
+    rm -rf ./tmp
+done
+
+echo "Done!"
